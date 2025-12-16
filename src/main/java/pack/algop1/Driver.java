338a253d0c1f2337e732d29e5b4f60becd545cb5
zqmhy <zqmhyt@gmail.com>
2025-12-16 22:21:42 +0200

    V1.2

--- src/main/java/pack/algop1/Driver.java
+++ src/main/java/pack/algop1/Driver.java
@@ -2,17 +2,14 @@ package pack.algop1;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 
 public class Driver extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Scene scene = new Scene(new Pane());
-        UI ui = new UI();
-        scene.setRoot(ui.p());
-        stage.setScene(scene);
+        stage.setScene(new Scene(new UI().p()));
         stage.setMaximized(true);
         stage.setTitle("An Intelligent Daily Task Scheduling System");
         stage.setResizable(false);
